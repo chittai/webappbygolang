b694c147ddc1ca7227311c6b5ef6b6f1cdbb12b2
布田遼平 <chittai1221@gmail.com>
2019-06-13 23:00:48 +0900

    Add OAuth2 by Github

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 	"net/http"
 	"path/filepath"
 	"sync"
+
+	"github.com/stretchr/gomniauth/providers/github"
+
+	"github.com/stretchr/gomniauth"
 )
 
 type templateHandler struct {
@@ -25,10 +29,16 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080", "localhost")
 	flag.Parse()
+	gomniauth.SetSecurityKey("1qaz2wsx")
+	gomniauth.WithProviders(
+		github.New("xx", "xx", "http://localhost:8080/auth/callback/github"),
+	)
+
 	r := newRoom()
 	//r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 
 	go r.run()
