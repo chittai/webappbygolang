889552e9298a17ea78295a4abb89c4f51c39c294
布田遼平 <chittai1221@gmail.com>
2019-06-20 23:44:32 +0900

    Add Avatar

---
+++ avatar_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestAuthAvatar(t *testing.T) {
+	var authAvatar AuthAvatar
+	client := new(client)
+	url, err := authAvatar.GetAvatarURL(client)
+	if err != ErrNoAvatarURL {
+		t.Error("値が存在しない場合、AuthAvatar.GetAvatarURLは" + "ErrNoAvatarURLを返すべきです")
+	}
+
+	testUrl := "http://url-to-avatar/"
+	client.userData = map[string]interface{}{"avatar_url": testUrl}
+	url, err = authAvatar.GetAvatarURL(client)
+
+	if err != nil {
+		t.Error("値が存在する場合、AuthAvatar.GetAvatarURLは" + "エラーを返すべきではありません")
+	} else {
+		if url != testUrl {
+			t.Error("AuthAvatar.GetAvatarURLは正しいURLを返すべきです")
+		}
+	}
+}
+
+func TestGravatarAvatar(t *testing.T) {
+	var gravatarAvatar GravatarAvatar
+	client := new(client)
+	client.userData = map[string]interface{}{"email": "MyEmailAddress@exmaple.com"}
+	url, err := gravatarAvatar.GetAvatarURL(client)
+	if err != nil {
+		t.Error("GravatarAvatar.GetAvatarURLはエラーを返すべきではありません")
+	}
+	if url != "//www.gravatar.com/avatar/0920f0cda7de04c5801c944d014674a7" {
+		t.Errorf("GravatarAvatar.GetAvatarURLが%sという誤った値を返しました", url)
+	}
+}
